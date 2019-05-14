33c955249857a2a405f332ebed1ff2070edb1766
Fiona Tolliday <fionatolliday@hotmail.com>
2019-05-14 13:29:24 +1000

    set up of Helper class

---
+++ src/Helper.java
@@ -0,0 +1,41 @@
+public class Helper {
+
+    public static void main() {
+
+
+
+    }
+
+    public int countTheVowels() {
+
+    }
+
+    public char middleCharacter() {
+        
+    }
+}
+
+
+
+
+//    Create a helper class which should have below methods :
+//
+//        1. Write a Java method to count all vowels in a string.
+//        Test Data:
+//        Input the string: Test
+//        Expected Output:Number of  Vowels in the string: 1
+//
+//        2. Write a Java method to display the middle character of a string. Go to the editor
+//        Note: a) If the length of the string is odd there will be two middle characters.
+//        b) If the length of the string is even there will be one middle character.
+//        Test Data:
+//        Input a string: 350
+//        Expected Output: The middle character in the string: 5
+//
+//        Have a another class which contains Main method and calls above two methods as per user input i.e.
+//
+//        Display :
+//        1. Count Vowels
+//        2. Find Middle Character
+//
+//        If user chooses 1 , it should call helper class vowel method .. if he chooses 2 than find middle.
